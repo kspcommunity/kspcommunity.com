122a68e2d7d072d3ecdb4af6aa6109d2db8aa6f7
G9 Aerospace <g9militants@gmail.com>
2024-03-18 11:47:37 +0530

    Massive UI Overhaul!

--- public/assets/js/global.js
+++ public/assets/js/global.js
@@ -35,3 +35,22 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .catch(error => console.error('Error loading footer:', error));
 });
+
+// Function to update background position based on scroll
+function updateBackgroundPosition() {
+    var scrollTop = window.scrollY;
+    var scrollSpeed = 0.3; // Adjust this value to change the scrolling speed
+    var backgroundPosition = 'center ' + (-scrollTop * scrollSpeed) + 'px';
+    document.body.style.backgroundPosition = backgroundPosition;
+}
+
+// Function to handle scroll event
+function handleScroll() {
+    requestAnimationFrame(updateBackgroundPosition);
+}
+
+// Initialize background position
+updateBackgroundPosition();
+
+// Add scroll event listener
+document.addEventListener("scroll", handleScroll);
