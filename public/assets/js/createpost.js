8886b1aedcde5817426bf11d2ba2b3d7fa603a71
Dave9123 <dave.turmawan@outlook.com>
2024-03-20 13:47:53 +0700

    a store unfction

---
+++ public/assets/js/createpost.js
@@ -0,0 +1,46 @@
+const uploadButton = document.getElementById("uploadButton");
+uploadButton.addEventListener("click", uploadFiles);
+
+function uploadFiles(event) {
+  event.preventDefault();
+
+  const titleInput = document.getElementById("title");
+  const descriptionInput = document.getElementById("description");
+  const imagesInput = document.getElementById("images");
+  const craftInput = document.getElementById("craft");
+
+  if (titleInput.value === "" || titleInput.value === undefined) {
+    return alert("Please enter a title.");
+  } else if (descriptionInput.value === "" || descriptionInput.value === undefined) {
+    return alert("Please enter a description.");
+  } else if (imagesInput.files.length < 1) {
+    return alert("Please select images to upload.");
+  } else if (craftInput.files.length < 1) {
+    return alert("Please select a craft file to upload.");
+  } else if (craftInput.length > 1) {
+    return alert("Please select only one craft file to upload.");
+  } else {
+    const formData = new FormData();
+    formData.append("title", titleInput.value);
+    formData.append("description", descriptionInput.value);
+    for (let i = 0; i < selectedFiles.length; i++) {
+      formData.append("images[]", imagesInput.files[i]);
+    }
+    formData.append("craft", craftInput.files[0]);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "/uploadFiles", true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status === 200) {
+          // Handle successful response from the server
+          console.log("Files uploaded successfully!");
+        } else {
+          // Handle error response from the server
+          console.error("Failed to upload files.");
+        }
+      }
+    };
+    xhr.send(formData);
+  }
+}
