aee8cf1a760e406be34971eacf2f85e2964d5647
Dave9123 <dave.turmawan@outlook.com>
2024-03-22 12:18:25 +0700

    Better checks

--- routes/createpost.js
+++ routes/createpost.js
@@ -24,40 +24,19 @@ const imageupload = multer({ storage: multer.diskStorage({
     }
 }), limits: { fileSize: 5 * 1024 * 1024 } });
 
-const craftupload = multer({ storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = path.join(__dirname, '..', 'uploads', 'craft');
-        fs.ensureDir(uploadPath, (err) => {
-            if (err) {
-                logger.error(`Error creating craft upload directory: ${err.message}`);
-                cb(err);
-            } else {
-                logger.info(`Craft upload directory created successfully at: ${uploadPath}`);
-                cb(null, uploadPath);
-            }
-        });
-    }
-}), limits: { fileSize: 5 * 1024 * 1024 } });
-
 router.post('/', imageupload.fields([{ name: 'images[]', maxCount: 10 }]), async (req, res) => {
     logger.info('Received POST request to /createpost');
     const title = req.body.title;
     const description = req.body.description;
     const images = req.files['images[]'];
     //const userId = req.session.userId;
-    logger.info(`Received files with title "${title}", ${images.length} images and 1 craft file`);
+    logger.info(`Received files with title "${title}", ${images.length} images`);
 
     try {
         if (!title || !description) {
             throw new Error('Title or description is missing');
-        } else if (images.length < 1 || !craft) {
-            throw new Error('Please select images and a craft file to upload');
         } else if (images.length > 10) {
             throw new Error('Please select 10 or fewer images to upload');
-        } else if (!craft.originalname.endsWith('.craft')) {
-            throw new Error('Please select a .craft file to upload');
-        } else if (craft.size > 5 * 1024 * 1024) {
-            throw new Error('Please select a craft file smaller than 5 MB to upload');
         }
 
         for (let i = 0; i < images.length; i++) {
