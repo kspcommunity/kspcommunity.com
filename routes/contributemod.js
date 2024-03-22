bb72cd7cef6d71936eaa09a26c80c63bcb9417f7
Dave9123 <dave.turmawan@outlook.com>
2024-03-22 21:12:59 +0700

    Character limits, report's title = 64 description 1024, contribute mod's title 32, description 256

--- routes/contributemod.js
+++ routes/contributemod.js
@@ -12,36 +12,37 @@ router.post('/',
     //    body('modLink').trim().escape()
     //],
     async (req, res) => {
-        // Check for validation errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
         const { modName, modLink } = req.body;
-        logger.info('Received mod contribution with name: ' + modName + 'and link: ' + modLink);
-
-        // Create the embed object
-        const embed = {
-            title: 'New Mod Contribution',
-            fields: [
-                { name: 'Mod Name', value: modName },
-                { name: 'Mod Link', value: modLink }
-            ]
-        };
-
-        // Send the embed to the Discord webhook
-        try {
-            await fetch(process.env.MOD_CONTRIBUTION_WEBHOOK, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ embeds: [embed] })
-            });
-            res.sendStatus(200);
-        } catch (error) {
-            logger.error('Error sending embed to Discord webhook:', error);
-            res.sendStatus(500);
+        if (modName === undefined || modLink === undefined) {
+            return res.status(400).json({ error: 'Missing modName or modLink' });
+        } else if (modName.length > 32 || modLink.length === 256) {
+            return res.status(400).json({ error: 'modName or modLink length is too long' });
+        } else {
+            logger.info('Received mod contribution with name: ' + modName + 'and link: ' + modLink);
+            const embed = {
+                title: 'New Mod Contribution',
+                fields: [
+                    { name: 'Mod Name', value: modName },
+                    { name: 'Mod Link', value: modLink }
+                ]
+            };
+            try {
+                await fetch(process.env.MOD_CONTRIBUTION_WEBHOOK, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ embeds: [embed] })
+                });
+                res.sendStatus(200);
+            } catch (error) {
+                logger.error('Error sending embed to Discord webhook:', error);
+                res.sendStatus(500);
+            }
         }
     }
 );
