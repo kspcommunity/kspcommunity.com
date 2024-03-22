bd1c9781e11d7994c06c26a3789cd08bad9669d8
Dave9123 <dave.turmawan@outlook.com>
2024-03-22 12:30:18 +0700

    yay upload

--- public/assets/js/createpost.js
+++ public/assets/js/createpost.js
@@ -1,18 +1,9 @@
-function uploadFiles(event) {
+function uploadCraft(event) {
   event.preventDefault();
 
-  const titleInput = document.getElementById("title");
-  const descriptionInput = document.getElementById("description");
-  const imagesInput = document.getElementById("images");
   const craftInput = document.getElementById("craft");
 
-  if (titleInput.value === "" || titleInput.value === undefined) {
-    return alert("Please enter a title.");
-  } else if (descriptionInput.value === "" || descriptionInput.value === undefined) {
-    return alert("Please enter a description.");
-  } else if (imagesInput.files.length < 1) {
-    return alert("Please select images to upload.");
-  } else if (craftInput.files.length < 1) {
+  if (craftInput.files.length < 1) {
     return alert("Please select a craft file to upload.");
   } else if (craftInput.length > 1) {
     return alert("Please select only one craft file to upload.");
@@ -20,15 +11,10 @@ function uploadFiles(event) {
     return alert("Please select a .craft file to upload.");
   } else {
     const formData = new FormData();
-    formData.append("title", titleInput.value);
-    formData.append("description", descriptionInput.value);
-    for (let i = 0; i < imagesInput.files.length; i++) {
-      formData.append("images[]", imagesInput.files[i]);
-    }
     formData.append("craft", craftInput.files[0]);
 
     const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/uploadfiles", true);
+    xhr.open("POST", "/uploadcraft", true);
     xhr.onreadystatechange = function () {
       if (xhr.readyState === XMLHttpRequest.DONE) {
         if (xhr.status === 200) {
@@ -45,5 +31,5 @@ function uploadFiles(event) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("uploadButton").addEventListener("click", uploadFiles);
+  document.getElementById("uploadButton").addEventListener("click", uploadCraft);
 });
