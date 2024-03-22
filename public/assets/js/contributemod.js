897f0ec29418b381f46f34ed8ca59990260a46f6
Dave9123 <dave.turmawan@outlook.com>
2024-03-22 21:12:59 +0700

    Character limits, report's title = 64 description 1024, contribute mod's title 32, description 256

--- public/assets/js/contributemod.js
+++ public/assets/js/contributemod.js
@@ -2,19 +2,27 @@ async function submitModLink(event) {
     event.preventDefault();
     const modName = document.getElementById("modName").value;
     const modLink = document.getElementById("modLink").value;
-    response = await fetch("/api/contributemod", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ modName, modLink }),
-    });
-    if (response.ok) {
-        alert("Thank you for your contribution!");
-        document.getElementById("modName").value = "";
-        document.getElementById("modLink").value = "";
+    if (modName.length > 32) {
+        alert("Mod name is too long. Please keep it under 32 characters.");
+        return;
+    } else if (modLink.length > 256) {
+        alert("Mod link is too long. Please keep it under 256 characters.");
+        return;
     } else {
-        alert("Something went wrong. Please try again later.");
+        response = await fetch("/api/contributemod", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ modName, modLink }),
+        });
+        if (response.ok) {
+            alert("Thank you for your contribution!");
+            document.getElementById("modName").value = "";
+            document.getElementById("modLink").value = "";
+        } else {
+            alert("Something went wrong. Please try again later.");
+        }
     }
 }
 document.addEventListener("DOMContentLoaded", function () {
