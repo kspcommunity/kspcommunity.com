8b2e5e11af670d6f4172c9de67fa9f590d0da5cb
Dave9123 <dave.turmawan@outlook.com>
2024-03-22 12:18:25 +0700

    Better checks

--- routes/uploadcraft.js
+++ routes/uploadcraft.js
@@ -6,24 +6,6 @@ const fs = require('fs-extra');
 const logger = require('../utilities/logger');
 const database = require('../database');
 
-const imageupload = multer({ storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = path.join(__dirname, '..', 'uploads', 'images');
-        fs.ensureDir(uploadPath, (err) => {
-            if (err) {
-                logger.error(`Error creating image uploads directory: ${err.message}`);
-                cb(err);
-            } else {
-                logger.info(`Images upload directory created successfully at: ${uploadPath}`);
-                cb(null, uploadPath);
-            }
-        });
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
-}), limits: { fileSize: 5 * 1024 * 1024 } });
-
 const craftupload = multer({ storage: multer.diskStorage({
     destination: function (req, file, cb) {
         const uploadPath = path.join(__dirname, '..', 'uploads', 'craft');
@@ -41,30 +23,15 @@ const craftupload = multer({ storage: multer.diskStorage({
 
 router.post('/', craftupload.fields([{ name: 'craft', maxCount: 1}]), async (req, res) => {
     logger.info('Received POST request to /uploadcraft');
-    const title = req.body.title;
-    const description = req.body.description;
-    const images = req.files['images[]'];
     const craft = req.files['craft'][0];
     //const userId = req.session.userId;
     logger.info(`Received files with title "${title}", ${images.length} images and 1 craft file`);
 
     try {
-        if (!title || !description) {
-            throw new Error('Title or description is missing');
-        } else if (images.length < 1 || !craft) {
-            throw new Error('Please select images and a craft file to upload');
-        } else if (images.length > 10) {
-            throw new Error('Please select 10 or fewer images to upload');
-        } else if (!craft.originalname.endsWith('.craft')) {
+        if (!craft.originalname.endsWith('.craft')) {
             throw new Error('Please select a .craft file to upload');
         } else if (craft.size > 5 * 1024 * 1024) {
             throw new Error('Please select a craft file smaller than 5 MB to upload');
-        }
-
-        for (let i = 0; i < images.length; i++) {
-            if (images[i].size > 5 * 1024 * 1024) {
-                throw new Error('Please select images smaller than 5 MB to upload');
-            }
         }
 
         // Save the files to the database
