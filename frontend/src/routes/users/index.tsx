0cd29ee81e37c04755086dc4d9af0c0ba62218d4
G9 Aerospace <g9militants@gmail.com>
2024-04-14 01:42:14 +0530

    Update backend index.js to include API route for displaying users

--- frontend/src/routes/users/index.tsx
+++ frontend/src/routes/users/index.tsx
@@ -1,106 +1,31 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
 
-import Infobox from "../../components/starter/infobox/infobox";
+interface User {
+  id: string;
+  name: string;
+  age: string;
+}
 
-// Import the background image
-import backgroundImage from "../../media/background/bg3.svg";
-
+export const useServerData = routeLoader$(async () => {
+  const response = await fetch('http://localhost:1234/api/users', {
+    headers: { Accept: 'application/json' },
+  });
+  return await response.json() as User[];
+});
+ 
 export default component$(() => {
+  const serverData = useServerData();
   return (
-    <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh' }}>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
-          </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
-
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
-
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.dev/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.dev/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
-      </div>
-    </div>
+    <section class="section bright">
+      <ul>
+        {serverData.value.map(user => (
+          <li key={user.id}>
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 });
-
-export const head: DocumentHead = {
-  title: "Users",
-  meta: [
-    {
-      name: "description",
-      content: "Users",
-    },
-  ],
-};
