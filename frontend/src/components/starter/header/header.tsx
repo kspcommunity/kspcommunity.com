b15ff00dbbda5daf8e3555cccce34cba59145158
G9 Aerospace <g9militants@gmail.com>
2024-04-13 12:57:28 +0530

    Moved to nodejs express entirely

--- frontend/src/components/starter/header/header.tsx
+++ frontend/src/components/starter/header/header.tsx
@@ -1,41 +1,18 @@
-import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
+import { component$} from "@builder.io/qwik";
 import styles from "./header.module.css";
+import ImgKSPC from "../../../media/kspcommunity.png";
 
 export default component$(() => {
+
   return (
-    <header class={styles.header}>
-      <div class={["container", styles.wrapper]}>
+    <header class={`${styles.header}`}>
+      <div class={styles.wrapper}>
         <div class={styles.logo}>
-          <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
-          </a>
+          {/* Use the imported image */}
+          <img src={ImgKSPC} alt="KSPC Logo" class={styles.logo} width="100" height="100" />
         </div>
-        <ul>
-          <li>
-            <a
-              href="https://qwik.dev/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.dev/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.dev/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
-            </a>
-          </li>
+        <ul class={styles.navList}>
+          <li><a href="/">Home</a></li>
         </ul>
       </div>
     </header>
