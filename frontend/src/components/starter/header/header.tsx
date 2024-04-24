13dc270228c53a13323b41e442d35a734526bf8f
G9 Aerospace <g9militants@gmail.com>
2024-04-24 18:18:58 +0530

    Starting Afresh!

--- frontend/src/components/starter/header/header.tsx
+++ frontend/src/components/starter/header/header.tsx
@@ -1,18 +1,41 @@
-import { component$} from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
+import { QwikLogo } from "../icons/qwik";
 import styles from "./header.module.css";
-import ImgKSPC from "../../../media/kspcommunity.png";
 
 export default component$(() => {
-
   return (
-    <header class={`${styles.header}`}>
-      <div class={styles.wrapper}>
+    <header class={styles.header}>
+      <div class={["container", styles.wrapper]}>
         <div class={styles.logo}>
-          {/* Use the imported image */}
-          <img src={ImgKSPC} alt="KSPC Logo" class={styles.logo} width="100" height="100" />
+          <a href="/" title="qwik">
+            <QwikLogo height={50} width={143} />
+          </a>
         </div>
-        <ul class={styles.navList}>
-          <li><a href="/">Home</a></li>
+        <ul>
+          <li>
+            <a
+              href="https://qwik.dev/docs/components/overview/"
+              target="_blank"
+            >
+              Docs
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://qwik.dev/examples/introduction/hello-world/"
+              target="_blank"
+            >
+              Examples
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://qwik.dev/tutorial/welcome/overview/"
+              target="_blank"
+            >
+              Tutorials
+            </a>
+          </li>
         </ul>
       </div>
     </header>
