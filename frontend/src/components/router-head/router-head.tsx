7b6dd81fca21d3cdac9ffb42fb369e78c7ce3c9f
G9 Aerospace <g9militants@gmail.com>
2024-04-24 22:14:21 +0530

    Update RouterHead component to include Open Graph tags and KSP Community logo

--- frontend/src/components/router-head/router-head.tsx
+++ frontend/src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
-
+import kspcommunitylogo from "../../media/kspcommunity.png";
 import { component$ } from "@builder.io/qwik";
 
 /**
@@ -11,11 +11,19 @@ export const RouterHead = component$(() => {
 
   return (
     <>
-      <title>{head.title}</title>
+      <title>KSPC - {head.title}</title>
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="theme-color" content="#cadbed" /> 
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+      {/* Open Graph tags */}
+      <meta property="og:title" content={`KSP Community - ${head.title}`} />
+      <meta property="og:description" content={head.meta.find((m) => m.name === "description")?.content} />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={loc.url.href} />
+      <meta property="og:image" content={kspcommunitylogo} />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
