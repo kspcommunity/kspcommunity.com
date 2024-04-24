9acfe660eacc7f544b42dbc7537b0c05fea9fdeb
G9 Aerospace <g9militants@gmail.com>
2024-04-24 22:50:40 +0530

    Update footer and sidebar styles

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,17 +1,13 @@
 import { component$ } from "@builder.io/qwik";
-import { useServerTimeLoader } from "../../routes/layout";
 import styles from "./footer.module.css";
 
 export default component$(() => {
-  const serverTime = useServerTimeLoader();
 
   return (
-    <footer>
+    <footer class={styles.footer}>
       <div class="container">
         <a href="https://www.builder.io/" target="_blank" class={styles.anchor}>
-          <span>Made with ♡ by Builder.io</span>
-          <span class={styles.spacer}>|</span>
-          <span>{serverTime.value.date}</span>
+          <span>KSP Community 2024</span>
         </a>
       </div>
     </footer>
--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -39,25 +39,12 @@ export default component$(() => {
           <a href="https://www.youtube.com/@G9AEROSPACEYT" target="_blank" rel="noopener">
             <img src="https://img.icons8.com/?size=256&id=19318&format=png" alt="Youtube" />
           </a>
-          <a href="https://github.com/g9militantsYT" target="_blank" rel="noopener">
+          <a href="https://github.com/kspcommunity" target="_blank" rel="noopener">
             <img src="https://img.icons8.com/?size=256&id=52539&format=png" alt="GitHub" />
           </a>
           <a href="https://discord.gg/gfzDMS7tQD" target="_blank" rel="noopener">
             <img src="https://img.icons8.com/?size=256&id=30998&format=png" alt="Discord" />
           </a>
-          <a href="https://steamcommunity.com/id/g9aerospace/" target="_blank" rel="noopener">
-            <img src="https://img.icons8.com/?size=256&id=zNqjI8XKkCv0&format=png" alt="Steam" />
-          </a>
-        </div>
-        
-        <div class={styles.sponsorContainer}>
-          <iframe class={styles.sponsor}
-            src="https://github.com/sponsors/g9militantsYT/button"
-            title="Sponsor g9militantsYT"
-            height="32"
-            width="80%"
-            style={{ border: '0', borderRadius: '6px' }}>
-          </iframe>
         </div>
       </div>
     </aside>
