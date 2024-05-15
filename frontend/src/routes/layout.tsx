54048bf7ea5fe26dea958405a95ab7f34546c8e2
g9aerospace <g9militants@gmail.com>
2024-05-15 09:15:45 +0530

    Reset

--- frontend/src/routes/layout.tsx
+++ frontend/src/routes/layout.tsx
@@ -2,8 +2,8 @@ import { component$, Slot, useStyles$ } from "@builder.io/qwik";
 import { routeLoader$ } from "@builder.io/qwik-city";
 import type { RequestHandler } from "@builder.io/qwik-city";
 
-import Sidebar from "../components/sidebar/sidebar";
-import Footer from "../components/footer/footer";
+import Header from "../components/starter/header/header";
+import Footer from "../components/starter/footer/footer";
 
 import styles from "./styles.css?inline";
 
@@ -28,7 +28,7 @@ export default component$(() => {
   useStyles$(styles);
   return (
     <>
-      <Sidebar />
+      <Header />
       <main>
         <Slot />
       </main>
