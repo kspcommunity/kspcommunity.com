3681ab7e338f5e874873d8c6838545b3738f5f49
g9aerospace <g9militants@gmail.com>
2024-05-15 09:15:45 +0530

    Reset

--- frontend/src/components/router-head/router-head.tsx
+++ frontend/src/components/router-head/router-head.tsx
@@ -1,5 +1,5 @@
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
-import kspcommunitylogo from "../../media/kspcommunity.png";
+
 import { component$ } from "@builder.io/qwik";
 
 /**
@@ -11,19 +11,11 @@ export const RouterHead = component$(() => {
 
   return (
     <>
-      <title>KSPC - {head.title}</title>
+      <title>{head.title}</title>
 
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta name="theme-color" content="#cadbed" /> 
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-
-      {/* Open Graph tags */}
-      <meta property="og:title" content={`KSP Community - ${head.title}`} />
-      <meta property="og:description" content={head.meta.find((m) => m.name === "description")?.content} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={loc.url.href} />
-      <meta property="og:image" content={kspcommunitylogo} />
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
@@ -34,11 +26,23 @@ export const RouterHead = component$(() => {
       ))}
 
       {head.styles.map((s) => (
-        <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
+        <style
+          key={s.key}
+          {...s.props}
+          {...(s.props?.dangerouslySetInnerHTML
+            ? {}
+            : { dangerouslySetInnerHTML: s.style })}
+        />
       ))}
 
       {head.scripts.map((s) => (
-        <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
+        <script
+          key={s.key}
+          {...s.props}
+          {...(s.props?.dangerouslySetInnerHTML
+            ? {}
+            : { dangerouslySetInnerHTML: s.script })}
+        />
       ))}
     </>
   );
