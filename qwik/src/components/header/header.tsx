7acf3322828cb63b5e28684f02d3b89aa4f4cba6
upayanmazumder <upayanm3@gmail.com>
2024-05-25 09:53:18 +0530

    🛠️ header.tsx -> Added new icons for Home, Create, and Settings

--- qwik/src/components/header/header.tsx
+++ qwik/src/components/header/header.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
 import QwikLogo from "../../media/icons/icon-72x72.png?jsx";
 import { Link } from "@builder.io/qwik-city";
+import { LuHome, LuFilePlus2, LuSettings } from "@qwikest/icons/lucide";
 import styles from "./header.module.css";
 
 export default component$(() => {
@@ -12,13 +13,13 @@ export default component$(() => {
       <h1>KSP Community</h1>
       <ul>
         <li>
-          <Link href="/">Home</Link>
+          <Link href="/"><LuHome/> Home</Link>
         </li>
         <li>
-          <Link href="/upload">Upload</Link>
+          <Link href="/create"><LuFilePlus2/> Create</Link>
         </li>
         <li>
-          <Link href="/settings">Settings</Link>
+          <Link href="/settings"><LuSettings/> Settings</Link>
         </li>
       </ul>
     </header>
