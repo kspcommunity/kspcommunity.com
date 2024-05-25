6c1fc14896a140b42cef2d9cdca3705e982eea39
upayanmazumder <upayanm3@gmail.com>
2024-05-25 08:41:02 +0530

    🛠️ .hintrc -> Added compatibility settings for CSS 🛠️ socials.tsx -> Updated links with title attributes 🛠️ global.css -> Added text-size-adjust property 🛠️ styles.css -> Removed unused CSS class

--- qwik/src/components/socials/socials.tsx
+++ qwik/src/components/socials/socials.tsx
@@ -7,15 +7,15 @@ import Youtube from "../../media/icons/youtube.svg?jsx";
 export default component$(() => {
   return (
     <div class={styles.socialIcons}>
-      <a href="https://discord.gg/X547TVKMqF" class={styles.iconLink}>
+      <a href="https://discord.gg/X547TVKMqF" class={styles.iconLink} title="Join us on Discord!">
         <Discord class={styles.icon} />
       </a>
 
-      <a href="https://www.instagram.com/g9aerospace/" class={styles.iconLink}>
+      <a href="https://www.instagram.com/g9aerospace/" class={styles.iconLink} title="Follow us on Instagram!">
         <Instagram class={styles.icon} />
       </a>
 
-      <a href="https://www.youtube.com/@G9AEROSPACEYT" class={styles.iconLink}>
+      <a href="https://www.youtube.com/@G9AEROSPACEYT" class={styles.iconLink} title="Subscribe to us on youtube!">
         <Youtube class={styles.icon} />
       </a>
     </div>
