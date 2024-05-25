81b969817c999034a2e787d5f1baa667473ce42d
Larssies <larsvanwylick@gmail.com>
2024-05-25 21:21:54 +0200

    build(deps): delete 1 file and update 2 files

---
+++ api/routes/signup.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { initializeApp } = require('firebase/app');
+const { getAuth, createUserWithEmailAndPassword } = require('firebase/auth');
+const firebaseConfig = require('../firebaseconfig.json');
+
+const router = express.Router();
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+
+router.post('/signup', (req, res) => {
+    const { email, password } = req.body;
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        res.status(200).send({ authenticated: true, credentials: userCredential.user });
+    })
+    .catch((error) => {
+        res.status(500).send({ authenticated: false, error: error.message });
+        console.error(error);
+    });
+});
+
+module.exports = router;
