50051b47af7a966002e140329a0041cd383361e0
upayanmazumder <upayanm3@gmail.com>
2024-06-06 22:11:07 +0530

    Basic signup route

--- qwik/src/routes/signup/index.tsx
+++ qwik/src/routes/signup/index.tsx
@@ -1,28 +1,73 @@
-import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { component$, useSignal } from '@builder.io/qwik';
+import { Form, routeAction$ } from '@builder.io/qwik-city';
 
-import Signup from "../../components/auth/signup";
-
-export default component$(() => {
-  return (
-    <>
-      <div class="container container-center container-spacing-xl">
-        
-        <Signup />
-        <br/>
-        Have an existing account? <Link href="/login">Login</Link>
-      </div>
-    </>
-  );
+export const usePostSignupAction = routeAction$((props) => {
+  console.log('usePostSignupAction', props);
+  const { email, password } = props;
+  console.log('email', email);
+  console.log('password', password);
+  const signupData = {
+    email,
+    password
+  };
+  fetch('https://api.kspcommunity.com/api/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(signupData),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Signup submitted successfully:', data);
+    // Handle success response if needed
+  })
+  .catch(error => {
+    console.error('Error submitting signup:', error);
+    // Handle error if needed
+  });
 });
 
-export const head: DocumentHead = {
-  title: "SignUp",
-  meta: [
-    {
-      name: "description",
-      content: "Signup for an account",
-    },
-  ],
-};
+export default component$(() => {
+  const emailSignal = useSignal('');
+  const passwordSignal = useSignal('');
+
+  const postSignupAction = usePostSignupAction();
+  return (
+    <section class='signup-section'>
+      <Form action={postSignupAction}>
+        <input
+          type='email'
+          name='email'
+          placeholder='Enter your email'
+          required
+          class='signup-input'
+          onChange$={(e) =>
+            (emailSignal.value = (e.target as HTMLInputElement).value)
+          }
+          value={emailSignal.value}
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='Enter your password'
+          required
+          class='signup-input'
+          onChange$={(e) =>
+            (passwordSignal.value = (e.target as HTMLInputElement).value)
+          }
+          value={passwordSignal.value}
+        />
+        <button
+          type='submit'
+          class='signup-submit-btn'
+          onClick$={() => {
+            emailSignal.value = '';
+            passwordSignal.value = '';
+          }}>
+          Submit
+        </button>
+      </Form>
+    </section>
+  );
+});
