6c4f5dc9858975ae375362ae8c7c2e0557060303
upayanmazumder <upayanm3@gmail.com>
2024-06-14 11:47:26 +0530

    🛠️ index.tsx -> Updated icons for login button 🛠️ header.tsx -> Updated icons for home, create, and settings links

--- qwik/src/components/header/auth/index.tsx
+++ qwik/src/components/header/auth/index.tsx
@@ -1,12 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
-import { LuUserCircle } from "@qwikest/icons/lucide";
+import { BsBoxArrowInRight } from "@qwikest/icons/bootstrap";
 
 export default component$(() => {
   return (
     <>
         <li>
-          <Link href="/auth/login"><LuUserCircle/> Login</Link>
+          <Link href="/auth/login"><BsBoxArrowInRight/> Login</Link>
         </li>
     </>
   );
