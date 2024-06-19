555373d587e2b6e0b3abc20e4b2f8af6fea4b4b0
upayanmazumder <upayanm3@gmail.com>
2024-06-20 00:35:03 +0530

    replaced link with a

--- qwik/src/components/header/auth/index.tsx
+++ qwik/src/components/header/auth/index.tsx
@@ -1,12 +1,11 @@
 import { component$ } from "@builder.io/qwik";
-import { Link } from "@builder.io/qwik-city";
 import { BsBoxArrowInRight } from "@qwikest/icons/bootstrap";
 
 export default component$(() => {
   return (
     <>
         <li>
-          <Link href="/auth/login"><BsBoxArrowInRight/> Login</Link>
+          <a href="/auth/login"><BsBoxArrowInRight/> Login</a>
         </li>
     </>
   );
